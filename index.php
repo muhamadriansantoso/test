e588cc5a36ce9549c0296dee6fef830f7025dc2a
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-06-30 19:45:16 +0700

    Commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <h1 style="color: #ed1942;">etstttt</h1>
 <h1>ABC</h1>
 <p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad ducimus saepe? Dicta quas fuga dolor, accusamus, voluptatum ratione in dolore quaerat deserunt adipisci architecto molestias unde iusto vitae repellat!</p>
+awdhawd
