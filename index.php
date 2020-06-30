0435d5094894399bfcad5dc4de2fff40be0b0f03
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-06-30 19:47:04 +0700

    Commit

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <h1>ABC</h1>
 <p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad ducimus saepe? Dicta quas fuga dolor, accusamus, voluptatum ratione in dolore quaerat deserunt adipisci architecto molestias unde iusto vitae repellat!</p>
 <div class="i">
-		<a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quasi! Quis quibusdam molestias officiis eaque aliquid, vitae quam corrupti, quidem ipsa vel optio accusantium dolorum ullam, nulla, voluptatibus atque aperiam!</a>
+		<a href="">Lorems ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quasi! Quis quibusdam molestias officiis eaque aliquid, vitae quam corrupti, quidem ipsa vel optio accusantium dolorum ullam, nulla, voluptatibus atque aperiam!</a>
 </div>
